7c56dff4b30459f6b59ffb7a395bda7f92a0b8cb
Aidan Moreau <moreau.ai@northeastern.edu>
2025-02-15 20:41:21 -0500

    Save

--- Assets/Collection Game/CollectionGameAssets/Scripts/PlayerController.cs
+++ Assets/Collection Game/CollectionGameAssets/Scripts/PlayerController.cs
@@ -5,10 +5,18 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody rb;
     public float speed = 5f;
+    public float jumpForce = 5f;
+    public bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        isGrounded = true;
+    }
+
+    void Update() 
+    {
+        Jump();
     }
 
     void FixedUpdate()
@@ -24,5 +32,20 @@ public class PlayerController : MonoBehaviour
         Vector3 movenment = new Vector3(horizontal, 0, vertical).normalized;
 
         rb.AddForce(movenment * speed);
+    }
+
+    void Jump() 
+    {
+        if(Imput.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    
+    }
+
+    void OnCollisionEnter(Collision collison)
+    {
+        ContactPoint contact = collison.contacts[0];
     }
 }
